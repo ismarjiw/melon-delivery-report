a2ffd922337320a56c30c5bbfd7118d04f35d31f
Ismarji Workman <ismarjiw@protonmail.com>
2022-11-02 19:20:30 -0400

    second commit

--- produce_summary.py
+++ produce_summary.py
@@ -9,7 +9,7 @@ def produce_summary():
         amount = float(words[2])
         count = int(words[1])
         melon = words[0]
-    print(f'Delivered {count} {melon} for total of ${amount}')
+        print(f'Delivered {count} {melon} for total of ${amount}')
 
     print("Day 2")
     the_file = open("um-deliveries-day-2.txt")
@@ -19,7 +19,7 @@ def produce_summary():
         amount = float(words[2])
         count = int(words[1])
         melon = words[0]
-    print(f'Delivered {count} {melon} for total of ${amount}')
+        print(f'Delivered {count} {melon} for total of ${amount}')
 
     print("Day 3")
     the_file = open("um-deliveries-day-3.txt")
@@ -29,6 +29,24 @@ def produce_summary():
         amount = float(words[2])
         count = int(words[1])
         melon = words[0]
-    print(f'Delivered {count} {melon} for total of ${amount}')
+        print(f'Delivered {count} {melon} for total of ${amount}')
 
 print(produce_summary())
+
+# def produce_summary_2(num_of_day, path):
+
+#     print(f"Day {num_of_day}")
+
+#     delivery_history = open(path)
+#     for line in delivery_history:
+#         line = line.rstrip()
+#         words = line.split('|')
+
+#         amount = float(words[2])
+#         count = int(words[1])
+#         melon = words[0]
+
+#         print(f'Delivered {count} {melon} for total of ${amount}')
+
+# print(produce_summary_2(1,"um-deliveries-day-3.txt"))
+
